74646a43e0687b4d816cc20167510f95d6d5fc98
tp12356twNccu <146693497+tp12356twNccu@users.noreply.github.com>
2024-05-09 17:57:21 +0800

    added CustomerFrame

---
+++ Group12/src/CustomerFrame.java
@@ -0,0 +1,10 @@
+
+
+/*
+ * Customers will be able to see how many sets of utensils they are renting, when they should return it and how much they will be charged in different scenarios
+ *       
+ */
+
+public class CustomerFrame {
+    
+}
---
+++ Group12/src/ShopFrame.java
@@ -0,0 +1,10 @@
+
+
+/*
+ * operations such as leasing, returning and charging are done by the shops 
+ */
+
+public class ShopFrame {
+
+    
+}
