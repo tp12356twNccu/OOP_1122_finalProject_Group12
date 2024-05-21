a0d743dcff20df31f89ca2d78d3e3ad4b9f4cbc7
tp12356twNccu <146693497+tp12356twNccu@users.noreply.github.com>
2024-05-21 21:20:08 +0800

    added registration and login function in FunctionManager

--- Group12/app/src/main/java/group12/FunctionManager.java
+++ Group12/app/src/main/java/group12/FunctionManager.java
@@ -24,17 +24,54 @@ public class FunctionManager {
 
     public void registration(String name, String password){
         // check if the name is already used
-        String checkNameStat = "SELECT Name FROM CUSTOMER;";
+        String checkNameStat = "SELECT COUNT(User_ID) FROM CUSTOMER WHERE Name = ?;";
         try {
             Connection conn = connectToDB();
-            Statement checkName = conn.createStatement();
-            ResultSet checkNameResult
+            
+            // check if the name is already used
+            PreparedStatement checkName = conn.prepareStatement(checkNameStat);
+            checkName.setString(1, name);
+            ResultSet checkNameResult = checkName.executeQuery();
+            if(checkNameResult.getInt(1) != 0){
+
+
+                System.out.println("The name is already used.");
+                return;
+            }
+
+            // insert the new user
+            PreparedStatement insertUser = conn.prepareStatement("INSERT INTO CUSTOMER(Name, Password, Set_Limit, Suspended) VALUES(?, ?, 1, 0);");
+            insertUser.setString(1, name);
+            insertUser.setString(2, password);
+            insertUser.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
 
     }
+
+    public boolean login(String name, String password){
+        String checkStat = "SELECT COUNT(User_ID) FROM CUSTOMER WHERE Name = ? AND Password = ?;";
+        try {
+            Connection conn = connectToDB();
+            PreparedStatement check = conn.prepareStatement(checkStat);
+            check.setString(1, name);
+            check.setString(2, password);
+            ResultSet checkResult = check.executeQuery();
+            if(checkResult.getInt(1) == 1){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    
 
 
 
